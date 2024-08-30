494c75f376d8093ac7f6da0594d9a2415de7b80e
CaballeroRojo <samuel.ramos.vergara@gmail.com>
2024-08-29 19:56:58 -0500

    estoy listo

---
+++ estudiante.java
@@ -0,0 +1,10 @@
+package estudiante;
+
+public class estudiante {
+	public String nombre ;
+	public String apellido;
+	public String correo;
+	public String documento;
+	public int edad;
+	
+}
---
+++ mein.java
@@ -0,0 +1,16 @@
+package estudiante;
+
+public class mein {
+	public static void main(String[] args) {
+	estudiante e = new estudiante();
+	
+	e.nombre= "juan carlos";
+	e.apellido= "londoño perez";
+	e.documento="345678";
+	e.correo = "juan@gmail.com";
+	e.edad = 19;
+	System.out.println(e.nombre+" "+e.apellido);
+	
+ }
+	
+}
